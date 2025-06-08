fa6a4b1be76218debfcfcd1ce2f5fbff8425bbc2
Simon Chabrier <simonchabrier@gmail.com>
2025-06-08 12:34:02 +0200

    fix Configuration.php getRootNode declaration

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -21,12 +21,7 @@ class Configuration implements ConfigurationInterface
     {
         $treeBuilder = new TreeBuilder('ch_cookie_consent');
 
-        if (method_exists($treeBuilder, 'getRootNode')) {
-            $rootNode = $treeBuilder->getRootNode();
-        } else {
-            // BC layer for symfony/config 4.1 and older
-            $rootNode = /* @scrutinizer ignore-deprecated */ $treeBuilder->root('ch_cookie_consent');
-        }
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
