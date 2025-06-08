17d5ee1f6fd92cd0b0de3865c77fd55171c51d75
Simon Chabrier <simonchabrier@gmail.com>
2025-06-08 11:47:26 +0200

    update CookieConsentController to use route attributes and fix setLocale method

--- Controller/CookieConsentController.php
+++ Controller/CookieConsentController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
@@ -76,7 +76,7 @@ class CookieConsentController
         string $cookieConsentPosition,
         TranslatorInterface $translator,
         bool $cookieConsentSimplified = false,
-        string $formAction = null
+        ?string $formAction = null
     ) {
         $this->twigEnvironment         = $twigEnvironment;
         $this->formFactory             = $formFactory;
@@ -153,7 +153,6 @@ class CookieConsentController
     {
         $locale = $request->get('locale');
         if (empty($locale) === false) {
-            $this->translator->setLocale($locale);
             $request->setLocale($locale);
         }
     }
