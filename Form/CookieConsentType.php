43b4569d1cbd769c272f7486cac68d066be56acf
Simon Chabrier <simonchabrier@gmail.com>
2025-06-20 14:50:29 +0200

    update form change choices for switch

--- Form/CookieConsentType.php
+++ Form/CookieConsentType.php
@@ -38,53 +38,39 @@ class CookieConsentType extends AbstractType
     /**
      * Build the cookie consent form.
      */
-    // public function buildForm(FormBuilderInterface $builder, array $options): void
-    // {
-    //     foreach ($this->cookieCategories as $category) {
-    //         $builder->add($category, ChoiceType::class, [
-    //             'expanded' => true,
-    //             'multiple' => false,
-    //             'data'     => $this->cookieChecker->isCategoryAllowedByUser($category) ? 'true' : 'false',
-    //             'choices'  => [
-    //                 ['ch_cookie_consent.yes' => 'true'],
-    //                 ['ch_cookie_consent.no' => 'false'],
-    //             ],
-    //         ]);
-    //     }
-
-    //     if ($this->cookieConsentSimplified === false) {
-    //         $builder->add('save', SubmitType::class, ['label' => 'ch_cookie_consent.save', 'attr' => ['class' => 'btn ch-cookie-consent__btn']]);
-    //     } else {
-    //         $builder->add('use_only_functional_cookies', SubmitType::class, ['label' => 'ch_cookie_consent.use_only_functional_cookies', 'attr' => ['class' => 'btn ch-cookie-consent__btn']]);
-    //         $builder->add('use_all_cookies', SubmitType::class, ['label' => 'ch_cookie_consent.use_all_cookies', 'attr' => ['class' => 'btn ch-cookie-consent__btn ch-cookie-consent__btn--secondary']]);
-
-    //         $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
-    //             $data = $event->getData();
-
-    //             foreach ($this->cookieCategories as $category) {
-    //                 $data[$category] = isset($data['use_all_cookies']) ? 'true' : 'false';
-    //             }
-
-    //             $event->setData($data);
-    //         });
-    //     }
-    // }
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // foreach ($this->cookieCategories as $category) {
+        //     $builder->add($category, ChoiceType::class, [
+        //         'expanded' => true,
+        //         'multiple' => false,
+        //         'data'     => $this->cookieChecker->isCategoryAllowedByUser($category) ? 'true' : 'false',
+        //         'choices'  => [
+        //             ['ch_cookie_consent.yes' => 'true'],
+        //             ['ch_cookie_consent.no' => 'false'],
+        //         ],
+        //     ]);
+        // }
+
         foreach ($this->cookieCategories as $category) {
             $builder->add($category, CheckboxType::class, [
-                // 'expanded' => true,
-                // 'multiple' => false,
-                'label_attr' => ['class' => 'checkbox-switch'],
+                'label' => "ch_cookie_consent.$category",
+                'data' => $this->cookieChecker->isCategoryAllowedByUser($category), // true/false par défaut
                 'attr' => ['class' => 'form-check-input'],
-                'data'     => $this->cookieChecker->isCategoryAllowedByUser($category) ? 'true' : 'false',
-                // 'choices'  => [
-                //     ['ch_cookie_consent.yes' => 'true'],
-                //     ['ch_cookie_consent.no' => 'false'],
-                // ],
+                'label_attr' => ['class' => 'checkbox-switch'],
+                'row_attr' => ['class' => 'form-check form-switch ch-cookie-consent__category-toggle'],
             ]);
         }
+
+        // on écoute l'événement PRE_SUBMIT pour gérer les données soumises et définir si ch_cookie_consent.yes ou ch_cookie_consent.no en fonction de l'état des cases à cocher
+        $builder->addEventListener(FormEvents::PRE_SUBMIT, function (FormEvent $event) {
+            $data = $event->getData();
+            foreach ($this->cookieCategories as $category) {
+                // Si la case est cochée, on met 'true', sinon 'false'
+                $data[$category] = isset($data[$category]) ? 'ch_cookie_consent.yes' : 'ch_cookie_consent.no';
+            }
+            $event->setData($data);
+        });
 
         if ($this->cookieConsentSimplified === false) {
             $builder->add('save', SubmitType::class, ['label' => 'ch_cookie_consent.save', 'attr' => ['class' => 'btn ch-cookie-consent__btn']]);
@@ -103,6 +89,7 @@ class CookieConsentType extends AbstractType
             });
         }
     }
+
 
 
 
