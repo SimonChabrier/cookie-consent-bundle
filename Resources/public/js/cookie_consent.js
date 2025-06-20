10997c5848bd3eed65225ab42dfaaa70a7a808c1
Simon Chabrier <simonchabrier@gmail.com>
2025-06-20 09:54:29 +0200

    update js

--- Resources/public/js/cookie_consent.js
+++ Resources/public/js/cookie_consent.js
@@ -1,60 +1,62 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const cookieConsent = document.querySelector('.ch-cookie-consent');
-  const manageBtn = document.querySelector('.ch-cookie-consent__manage-btn');
-  const cookieConsentForm = document.querySelector('.ch-cookie-consent__form');
-  const cookieConsentFormBtn = document.querySelectorAll('.ch-cookie-consent__btn');
-  const cookieConsentCategoryDetails = document.querySelector('.ch-cookie-consent__category-group');
-  const cookieConsentCategoryDetailsToggle = document.querySelector('.ch-cookie-consent__toggle-details');
+document.addEventListener("DOMContentLoaded", () => {
+  const cookieConsent = document.querySelector(".ch-cookie-consent");
+  const manageBtn = document.querySelector(".ch-cookie-consent__manage-btn");
+  const cookieConsentForm = document.querySelector(".ch-cookie-consent__form");
+  const cookieConsentFormBtn = document.querySelectorAll(".ch-cookie-consent__btn");
+  const cookieConsentCategoryDetails = document.querySelector(".ch-cookie-consent__category-group");
+  const cookieConsentCategoryDetailsToggle = document.querySelector(".ch-cookie-consent__toggle-details");
 
   // If cookie consent is direct child of body, assume it should be placed on top of the site pushing down the rest of the website
-  if (cookieConsent && cookieConsent.parentNode.nodeName === 'BODY') {
-    if (cookieConsent.classList.contains('ch-cookie-consent--top')) {
+  if (cookieConsent && cookieConsent.parentNode.nodeName === "BODY") {
+    if (cookieConsent.classList.contains("ch-cookie-consent--top")) {
       document.body.style.marginTop = `${cookieConsent.offsetHeight}px`;
 
-      cookieConsent.style.position = 'absolute';
-      cookieConsent.style.top = '0';
-      cookieConsent.style.left = '0';
+      cookieConsent.style.position = "absolute";
+      cookieConsent.style.top = "0";
+      cookieConsent.style.left = "0";
     } else {
       document.body.style.marginBottom = `${cookieConsent.offsetHeight}px`;
 
-      cookieConsent.style.position = 'fixed';
-      cookieConsent.style.bottom = '0';
-      cookieConsent.style.left = '0';
+      cookieConsent.style.position = "fixed";
+      cookieConsent.style.bottom = "0";
+      cookieConsent.style.left = "0";
     }
   }
 
   if (manageBtn) {
-    manageBtn.addEventListener('click', () => {
-      manageBtn.style.display = 'none';
+    manageBtn.addEventListener("click", () => {
+      console.warn("Manage cookies button clicked");
+      console.warn("cookieConsent", cookieConsent);
+      manageBtn.style.display = "none";
       if (cookieConsent) {
-        cookieConsent.style.display = 'block';
+        cookieConsent.style.display = "block";
       }
     });
   }
 
   if (cookieConsentForm) {
     // Submit form via ajax
-    cookieConsentFormBtn.forEach((btn) => {
+    cookieConsentFormBtn.forEach(btn => {
       btn.addEventListener(
-        'click',
-        (event) => {
+        "click",
+        event => {
           event.preventDefault();
 
           const formAction = cookieConsentForm.action ? cookieConsentForm.action : location.href;
 
           fetch(formAction, {
-            method: 'POST',
+            method: "POST",
             headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              'X-Requested-With': 'XMLHttpRequest',
+              "Content-Type": "application/x-www-form-urlencoded",
+              "X-Requested-With": "XMLHttpRequest"
             },
-            body: serializeForm(cookieConsentForm, event.target),
-          }).then((response) => {
+            body: serializeForm(cookieConsentForm, event.target)
+          }).then(response => {
             if (response.ok) {
-              cookieConsent.style.display = 'none';
-              if (manageBtn) manageBtn.style.display = 'block';
-              const buttonEvent = new CustomEvent('cookie-consent-form-submit-successful', {
-                detail: event.target,
+              cookieConsent.style.display = "none";
+              if (manageBtn) manageBtn.style.display = "block";
+              const buttonEvent = new CustomEvent("cookie-consent-form-submit-successful", {
+                detail: event.target
               });
               document.dispatchEvent(buttonEvent);
               // Force <head> refresh to update when using UXTurbo or Turbo
@@ -72,22 +74,18 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   if (cookieConsentCategoryDetails && cookieConsentCategoryDetailsToggle) {
-    cookieConsentCategoryDetailsToggle.addEventListener('click', () => {
-      const detailsIsHidden = cookieConsentCategoryDetails.style.display !== 'block';
-      cookieConsentCategoryDetails.style.display = detailsIsHidden ? 'block' : 'none';
-      cookieConsentCategoryDetailsToggle
-        .querySelector('.ch-cookie-consent__toggle-details-hide')
-        .style.display = detailsIsHidden ? 'block' : 'none';
-      cookieConsentCategoryDetailsToggle
-        .querySelector('.ch-cookie-consent__toggle-details-show')
-        .style.display = detailsIsHidden ? 'none' : 'block';
+    cookieConsentCategoryDetailsToggle.addEventListener("click", () => {
+      const detailsIsHidden = cookieConsentCategoryDetails.style.display !== "block";
+      cookieConsentCategoryDetails.style.display = detailsIsHidden ? "block" : "none";
+      cookieConsentCategoryDetailsToggle.querySelector(".ch-cookie-consent__toggle-details-hide").style.display = detailsIsHidden ? "block" : "none";
+      cookieConsentCategoryDetailsToggle.querySelector(".ch-cookie-consent__toggle-details-show").style.display = detailsIsHidden ? "none" : "block";
     });
   }
 });
 
 const serializeForm = (form, clickedButton) => {
   const formData = new FormData(form);
-  formData.append(clickedButton.getAttribute('name'), '');
+  formData.append(clickedButton.getAttribute("name"), "");
 
   const params = new URLSearchParams();
   for (const [key, value] of formData.entries()) {
@@ -97,9 +95,9 @@ const serializeForm = (form, clickedButton) => {
   return params.toString();
 };
 
-if (typeof window.CustomEvent !== 'function') {
+if (typeof window.CustomEvent !== "function") {
   const CustomEventPolyfill = (event, params = { bubbles: false, cancelable: false, detail: undefined }) => {
-    const evt = document.createEvent('CustomEvent');
+    const evt = document.createEvent("CustomEvent");
     evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
     return evt;
   };
